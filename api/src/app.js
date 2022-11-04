492801de8ffda5d8e93e1122d4f398186935c521
lubermudezz <bermudezluciana@hotmail.com>
2022-11-04 14:35:49 -0300

    cors

--- api/src/app.js
+++ api/src/app.js
@@ -3,6 +3,9 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
+var cors = require('cors')
+
+
 
 require('./db.js');
 
@@ -10,12 +13,13 @@ const server = express();
 
 server.name = 'API';
 
+server.use(cors()) // Use this after the variable declaration
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://dog-s-app-production.up.railway.app'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
